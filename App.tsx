14385848d40dfdc788864f0bdc74e1f6a2cf068b
Kemuel Batista <kemuellima20@gmail.com>
2024-01-22 17:27:07 -0300

    feat: eslint config and native base install

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { Text, View, StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { View, StatusBar } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }) 
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#202024' }}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ?  <Text>Hello world!</Text> : <View />}
-    </View>
-  );
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
+  )
 }
