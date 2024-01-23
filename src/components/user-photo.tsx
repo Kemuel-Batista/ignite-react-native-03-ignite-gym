65362c58c050fd04673c6e33c872c00156f53610
Kemuel Batista <kemuellima20@gmail.com>
2024-01-23 10:03:50 -0300

    feat: finalizando as interfaces

---
+++ src/components/user-photo.tsx
@@ -0,0 +1,19 @@
+import { Image, IImageProps } from 'native-base'
+
+type Props = IImageProps & {
+  size: number
+}
+
+export function UserPhoto({ size, ...rest }: Props) {
+  return (
+    <Image
+      w={size}
+      h={size}
+      rounded="full"
+      alt=""
+      borderWidth={2}
+      borderColor="gray.400"
+      {...rest}
+    />
+  )
+}
